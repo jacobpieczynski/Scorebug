0d738038ece38c13d00e5a0d953082056c4447d9
Jacob Pieczynski <146683242+jacobpieczynski@users.noreply.github.com>
2024-03-20 18:25:23 +0000

    create array of games

--- beautiful_soup_test.py
+++ beautiful_soup_test.py
@@ -1,6 +1,6 @@
 # https://realpython.com/python-web-scraping-practical-introduction/
-
 from bs4 import BeautifulSoup
+from Game import *
 from urllib.request import urlopen
 
 url = 'http://olympus.realpython.org/profiles/dionysus' # Tutorial URL
@@ -10,15 +10,16 @@ url = 'https://www.espn.com/mlb/scoreboard/_/date/20240320' # MLB Scoreboard
 urls = ['https://www.espn.com/nhl/scoreboard/_/date/20240320', 'https://www.espn.com/nba/scoreboard/_/date/20240320', 'https://www.espn.com/mlb/scoreboard/_/date/20240320']
 
 def main():
-
+    games = dict()
     for url in urls:
         page = urlopen(url)
         html = page.read().decode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
-        games = len(soup.find_all(class_="Scoreboard"))
+        game_ct = len(soup.find_all(class_='Scoreboard'))
         league = url.split('/')[3]
-        print(f'League: {league.upper()} has {games} games today.')
-    #print(soup.find_all(class_="Scoreboard"))
+        games[league] = soup.find_all(class_='Scoreboard')
+        print(f'League: {league.upper()} has {game_ct} games today.')
+    #print(soup.find_all(class_='Scoreboard'))
     #print(soup.prettify())
-
+    print(games.keys())
 main()
